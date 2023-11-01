ee09d0ef496d0a272d6f9321b193f9dce37cd877
siskanuriaprilia <aprilias878@gmail.com>
2023-11-01 21:34:29 +0700

    modifikasi5

--- Bioskop27.java
+++ Bioskop27.java
@@ -26,5 +26,11 @@ public class Bioskop27 {
         {
             System.out.println("panjang  baris: " + barisPenonton.length);
         }
+
+        System.out.println("Penonton pada baris ke-3: ");
+        for (int i = 0; i < penonton[2].length; i++)
+        {
+           System.out.println(penonton[2][i]);
+        }
     }
 }
