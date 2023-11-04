0a11874cf65bb4b52f51740c67f67b772c4714e9
siskanuriaprilia <aprilias878@gmail.com>
2023-11-04 12:45:21 +0700

    modifikasi1

--- BioskopWithScanner27.java
+++ BioskopWithScanner27.java
@@ -9,61 +9,14 @@ public class BioskopWithScanner27 {
         String nama, next;
         String[][] penonton = new String[4][2];
 
-        loop = 0;
-        while (loop == 0) {
-            System.out.println("Menu:");
+        while (loop != 3) {
+            System.out.println("Menu: ");
             System.out.println("1. Input data penonton");
             System.out.println("2. Tampilkan daftar penonton");
             System.out.println("3. Exit");
-            System.out.print("Pilih menu (1/2/3): ");
+            System.out.println("Pilih menu (1/2/3)");
             int menu = input27.nextInt();
             input27.nextLine();
-
-            switch (menu) 
-            {
-                case 1:
-                    System.out.println("Masukkan nama penonton: ");
-                    nama = input27.nextLine();
-                break;
-
-                case 2:
-                        System.out.println("Daftar Penonton:");
-                        for (int i = 0; i < 4; i++) {
-                            for (int j = 0; j < 2; j++) 
-                            {
-                                if (penonton[i][j] == null) 
-                                {
-                                    System.out.print("*** ");
-                                } 
-                                else 
-                                {
-                                    System.out.print(penonton[i][j] + " ");
-                                }
-                            }
-                            System.out.println();
-                        }
-                        break;
-    
-                    case 3:
-                        System.out.println("Terima kasih. Program selesai.");
-                        loop = 1;
-                        break;
-                    default:
-                        System.out.println("Menu tidak valid. Silakan pilih menu yang sesuai.");
-                }
-    
-                System.out.print("Lanjutkan? (y/n): ");
-                next = sc.nextLine();
-    
-                if (next.equalsIgnoreCase("n")) {
-                    break;
-                }
-            }
         }
     }
-    }
 }
-
-        
-
-        
